76cce23b851a2f8b4e365ca4b727506cc32ada3a
snimmo <stephennimmo@gmail.com>
2023-12-05 08:20:23 -0600

    project init

---
+++ src/main/java/com/od2t/GreetingConfig.java
@@ -0,0 +1,12 @@
+package com.od2t;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
